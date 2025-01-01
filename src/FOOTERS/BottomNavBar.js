b617fc5802d6d48e46ab96949aa055b961eef2e0
rkverma2022 <rajkumarverma70790@gmail.com>
2025-01-01 13:22:22 +0530

    new repo

--- src/FOOTERS/BottomNavBar.js
+++ src/FOOTERS/BottomNavBar.js
@@ -7,14 +7,23 @@ export default function BottomBar() {
     <footer>
       <div className="copyright">
         <h3>
-        An Open Source Project: 
-          <Link to="https://github.com/S-H-E-R-Development/Machine-Learning-A-2-Z-WEBSITE" target="_blank">
-          GitHub-React
+          An Open Source Project:
+          <Link
+            to="https://github.com/S-H-E-R-Development/Machine-Learning-A-2-Z-WEBSITE"
+            target="_blank"
+            style={{ textDecoration: "underline" }}
+          >
+            &nbsp; GitHub-React &nbsp;
           </Link>
-          <Link to="https://github.com/Machine-Learning-A-2-Z/00---Basics-of-ML" target="_blank">
-          GitHub-ML
+          <Link
+            to="https://github.com/Machine-Learning-A-2-Z/00---Basics-of-ML"
+            target="_blank"
+            style={{ textDecoration: "underline" }}
+          >
+            GitHub-ML
           </Link>
-        </h3> 
+
+        </h3>
       </div>
     </footer>
   );
