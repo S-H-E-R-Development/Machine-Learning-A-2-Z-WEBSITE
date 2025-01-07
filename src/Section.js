58b2ec0178053cf44804e3caabfffce5772a8461
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2025-01-07 12:42:06 +0530

    initial commit

--- src/Section.js
+++ src/Section.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Section = ({ title, content }) => {
     return (
         <section className="section">
-            <h2>{title}</h2>
+            <h2 className='font-bold text-2xl'>{title}</h2>
             <div>{content}</div>
         </section>
     );
