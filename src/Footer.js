c2fba7694dcc7db9a623c3759d8098b39d4d07f8
rkverma2022 <rajkumarverma70790@gmail.com>
2025-12-09 12:58:10 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>For any questions or support, please reach out to <a href="mailto:rajkumarverma70790@gmail.com">rajkumarverma70790@gmail.com</a>.</p>
+            <p>For any questions or support, please reach out to <a href="mailto:rajkumarverma70790@gmail.com">rkverma.sher@gmail.com</a>.</p>
         </footer>
     );
 };
